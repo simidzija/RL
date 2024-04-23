77acaf8faab0f6fac8ea131caa557b1d72ed338d
Petar Simidzija <psimidzija@gmail.com>
2024-04-23 11:09:38 -0700

    Added model.py

---
+++ model.py
@@ -0,0 +1,26 @@
+import torch
+from torch.distributions import Categorical
+import torch.nn as nn
+
+class MLP(nn.Module):
+    def __init__(self, neurons):
+        super().__init__()
+        layers = [nn.Linear(neurons[0], neurons[1])]
+        for i, o in zip(neurons[1:-1], neurons[2:]):
+            layers.append(nn.Tanh())
+            layers.append(nn.Linear(i, o))
+        self.layers = nn.Sequential(*layers)
+
+    def forward(self, x):
+        return self.layers(x)
+    
+
+class Policy(MLP):
+    def __init__(self, neurons):
+        super().__init__(neurons)
+
+    def get_action(self, obs):
+        obs = torch.as_tensor(obs)
+        logits = self(obs)
+        dist = Categorical(logits=logits)
+        return dist.sample().item()
